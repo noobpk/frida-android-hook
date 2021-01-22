10516d27ff48e95f3168c951fb75d6540d1c81f8
noobpk <ltp.noobpk@gmail.com>
2021-01-22 13:46:31 +0700

    update core hook.py add lib/listapp.py

--- hook.py
+++ hook.py
@@ -9,26 +9,85 @@ import shutil
 import tempfile
 import subprocess
 import re
+import fnmatch
+import shlex
+import subprocess
+import psutil
 
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
 from tqdm import tqdm
 import traceback
+from lib.listapp import *
+from lib.checkversion import *
+from lib.log import *
 
 print ('''\033[1;31m \n
-_|      _|                      _|                  _|      
-_|_|    _|    _|_|      _|_|    _|_|_|    _|_|_|    _|  _|  
-_|  _|  _|  _|    _|  _|    _|  _|    _|  _|    _|  _|_|    
-_|    _|_|  _|    _|  _|    _|  _|    _|  _|    _|  _|  _|  
-_|      _|    _|_|      _|_|    _|_|_|    _|_|_|    _|    _|
-        https://noobpk.github.io          _|                 
-Trace Class/Func & Modify Return Value    _|   #noobteam
+                    _           _     _   _    _             _    
+    /\             | |         (_)   | | | |  | |           | |   
+   /  \   _ __   __| |_ __ ___  _  __| | | |__| | ___   ___ | | __
+  / /\ \ | '_ \ / _` | '__/ _ \| |/ _` | |  __  |/ _ \ / _ \| |/ /
+ / ____ \| | | | (_| | | | (_) | | (_| | | |  | | (_) | (_) |   < 
+/_/    \_\_| |_|\__,_|_|  \___/|_|\__,_| |_|  |_|\___/ \___/|_|\_\\\t
+    Trace Class/Func & Modify Return Value          #noobteam
 ''')
 
 print ("\033[1;34m[*]___author___: @noobpk\033[1;37m")
 print ("\033[1;34m[*]___version___: 1.1\033[1;37m")
 print ("")
+
+def check_platform():
+    try:
+        platforms = {
+        'linux'  : 'Linux',
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'darwin' : 'OS X',
+        'win32'  : 'Windows'
+        }
+        if sys.platform not in platforms:
+            logger.error("[x_x] Your platform currently does not support.")
+            sys.exit(0)
+    except Exception as e:
+        logger.error("[x_x] Something went wrong, please check your error message.\n Message - {0}".format(e))
+
+def handle_del_log():
+    try:
+        pwd = os.getcwd()
+        path = pwd + '/errors.log'
+        file_stats = os.stat(path)
+        if (file_stats.st_size > 1024000000): #delete errors.log if file size > 1024 MB
+            os.remove(path)
+        else:
+            return True
+    except Exception as e:
+        logger.error("[x_x] Something went wrong when clear error log. Please clear error log manual.\n Message - {0}".format(e))
+
+def check_ps_for_win32():
+    try:
+        if sys.platform == "win32":
+            PROCESSNAME = "iTunes.exe"
+            for proc in psutil.process_iter():
+                try:
+                    if proc.name() == PROCESSNAME:
+                        return True
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+                    pass
+            return sys.exit(logger.error("[x_x] Please install iTunes on MicrosoftStore or run iTunes frist."))              
+    except Exception as e:
+        logger.error("[x_x] Something went wrong, please check your error message.\n Message - {0}".format(e))
+
+def run():
+    #check platform support
+    check_platform()
+    #check python version
+    if sys.version_info < (3, 0):
+        logger.error("[x_x] iOS hook requires Python 3.x")
+        sys.exit(1)
+    else:
+        handle_del_log()
+        main()
 
 def start_frida_server():
     fs = "/data/local/tmp/frida-server*"
@@ -64,121 +123,50 @@ def stop_frida_server():
     else:
         print("\033[1;33m[!] Frida Server Not Start\033[1;33m")
 
-def get_usb_iphone():
-    Type = 'usb'
-    if int(frida.__version__.split('.')[0]) < 12:
-        Type = 'tether'
-    device_manager = frida.get_device_manager()
-    changed = threading.Event()
-
-    def on_changed():
-        changed.set()
-
-    device_manager.on('changed', on_changed)
-
-    device = None
-    while device is None:
-        devices = [dev for dev in device_manager.enumerate_devices() if dev.type == Type]
-        if len(devices) == 0:
-            print('[?] Waiting for USB device...')
-            changed.wait()
-        else:
-            device = devices[0]
-
-    device_manager.off('changed', on_changed)
-
-    return device
-
-def compare_applications(a, b):
-    a_is_running = a.pid != 0
-    b_is_running = b.pid != 0
-    if a_is_running == b_is_running:
-        if a.name > b.name:
-            return 1
-        elif a.name < b.name:
-            return -1
-        else:
-            return 0
-    elif a_is_running:
-        return -1
-    else:
-        return 1
-
-def cmp_to_key(mycmp):
-    """Convert a cmp= function into a key= function"""
-
-    class K:
-        def __init__(self, obj):
-            self.obj = obj
-
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-
-    return K
-
-def get_applications(device):
-    try:
-        applications = device.enumerate_applications()
-    except Exception as e:
-        sys.exit('\033[1;31m[-] Failed to enumerate applications: %s\033[1;31m' % e)
-
-    return applications
-
-def list_applications(device):
-    applications = get_applications(device)
-
-    if len(applications) > 0:
-        pid_column_width = max(map(lambda app: len('{}'.format(app.pid)), applications))
-        name_column_width = max(map(lambda app: len(app.name), applications))
-        identifier_column_width = max(map(lambda app: len(app.identifier), applications))
-    else:
-        pid_column_width = 0
-        name_column_width = 0
-        identifier_column_width = 0
-
-    header_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
-        identifier_column_width) + 's'
-    print(header_format % ('PID', 'Name', 'Identifier'))
-    print('%s  %s  %s' % (pid_column_width * '-', name_column_width * '-', identifier_column_width * '-'))
-    line_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
-        identifier_column_width) + 's'
-    for application in sorted(applications, key=cmp_to_key(compare_applications)):
-        if application.pid == 0:
-            print(line_format % ('-', application.name, application.identifier))
-        else:
-            print(line_format % (application.pid, application.name, application.identifier))
-
 def main():
-    usage = "Usage: python3 %prog [options] arg\n\rExample: python3 hook.py -p com.android.vending -s trace_class.js"
-    parser = optparse.OptionParser(usage,add_help_option=False)
-    parser.add_option('-h', "--help", action="help", dest="help", help="Show basic help message and exit")
-    parser.add_option("-p", "--package", dest="package",
-                    help="Bundle identifier of the target app", metavar="PACKAGE", action="store", type="string")
-    parser.add_option("-s", "--script", dest="script",
-                    help="Frida Script Hooking", metavar="SCIPRT.JS")
-    parser.add_option("--fs-start",
-                    action="store_true", help="Start frida server", dest="startfs")
-    parser.add_option("--fs-stop",
-                    action="store_true", help="Stop frida server", dest="stopfs")
-    parser.add_option("--listapp",
-                    action="store_true", help="List the installed apps", dest="listapp")    
-    options, args = parser.parse_args()
     try:
+
+        usage = "Usage: python3 %prog [options] arg\n\rExample: python3 hook.py -p com.android.vending -s trace_class.js"
+        parser = optparse.OptionParser(usage,add_help_option=False)
+        info = optparse.OptionGroup(parser,"Information")
+        quick = optparse.OptionGroup(parser,"Quick Method")
+
+        parser.add_option('-h', "--help", action="help", dest="help", help="Show basic help message and exit")
+        #Using options -p(--package) for spawn application and load script
+        parser.add_option("-p", "--package", dest="package",
+                        help="Identifier of the target app", metavar="PACKAGE", action="store", type="string")
+        #Using options -n(--name) for attach script to application is running
+        parser.add_option("-n", "--name", dest="name",
+                        help="Name of the target app", metavar="NAME", action="store", type="string")
+
+        parser.add_option("-s", "--script", dest="script",
+                        help="Frida Script Hooking", metavar="SCIPRT.JS")
+        parser.add_option("-d", "--dump", action="store_true", help="Dump decrypt application.ipa", dest="dump")
+        parser.add_option("-c", "--check-version", action="store_true", help="Check iOS hook for the newest version", dest="checkversion")
+        parser.add_option("-u", "--update", action="store_true", help="Update iOS hook to the newest version", dest="update")
+        quick.add_option("-m", "--method", dest="method", type="choice", choices=['app-static','bypass-root','bypass-ssl','i-url-req','i-crypto'],
+                        help="__app-static: Static Ananlysis Application(-n)\n\n\r\r__bypass-jb: Bypass Root Detection(-p)\n\n\r\r\r\r\r\r__bypass-ssl: Bypass SSL Pinning(-p)\n\n\n\n\n\n\n\n\n\r\r\r\r\r\r__i-nw-req: Intercept NetworkRequest in App(-p)\n\n\n\n\n\n\n\n\n\r\r\r\r\r\r__i-crypto: Intercept Crypto in App(-p)", metavar="app-static / bypass-root / bypass-ssl / i-nw-req / i-crypto")
+        info.add_option("--fs-start",
+                        action="store_true", help="Start frida server", dest="startfs")
+        info.add_option("--fs-stop",
+                        action="store_true", help="Stop frida server", dest="stopfs")
+        info.add_option("--list-apps",
+                        action="store_true", help="List the installed apps", dest="listapp") 
+
+        parser.add_option_group(info)
+        parser.add_option_group(quick)
+
+        options, args = parser.parse_args()
+        
+        methods = [
+            "method/_.js", #0
+            "method/static_analysis.js", #1
+            "method/bypass_ssl.js", #2
+            "method/bypass_root.js", #3
+            "method/intercept_nw_request.js", #4
+            "method/intercept_crypto.js" #5
+        ]
+
         if options.startfs:
             get_usb_iphone()
             start_frida_server()
@@ -191,28 +179,141 @@ def main():
             device = get_usb_iphone()
             list_applications(device)
 
+        #Attaching script to application
+        elif options.name and options.script:
+            if os.path.isfile(options.script):
+                logger.info('[*] Attaching: ' + options.name)
+                logger.info('[*] Script: ' + options.script)
+                time.sleep(2)
+                process = frida.get_usb_device().attach(options.name)
+                hook = open(options.script, 'r')
+                script = process.create_script(hook.read())
+                script.load()
+                sys.stdin.read()
+            else:
+                logger.error('[?] Script not found!')
+
+        #Spawning application and load script
         elif options.package and options.script:
-            print('[*] Spawning: ' + options.package)
-            print('[*] Script: ' + options.script)
-            pid = frida.get_usb_device().spawn(options.package)
-            session = frida.get_usb_device().attach(pid)
-            hook = open(options.script, 'r')
-            script = session.create_script(hook.read())
-            script.load()
-            frida.get_usb_device().resume(pid)
-            sys.stdin.read()
-            print('---------------Hook---Done---------------')
+            if os.path.isfile(options.script):
+                logger.info('[*] Spawning: ' + options.package)
+                logger.info('[*] Script: ' + options.script)
+                time.sleep(2)
+                pid = frida.get_usb_device().spawn(options.package)
+                session = frida.get_usb_device().attach(pid)
+                hook = open(options.script, 'r')
+                script = session.create_script(hook.read())
+                script.load()
+                frida.get_usb_device().resume(pid)
+                sys.stdin.read()
+            else:
+                logger.error('[?] Script not found!')
+
+        #Bypass jailbreak
+        elif options.package and options.method == "bypass-root":
+            method = methods[3]
+            logger.warning('[!] The Method Is Updating!!')
+            # if os.path.isfile(method):
+            #     logger.info('[*] Bypass Jailbreak: ')
+            #     logger.info('[*] Spawning: ' + options.package)
+            #     logger.info('[*] Script: ' + method)
+            #     time.sleep(2)
+            #     pid = frida.get_usb_device().spawn(options.package)
+            #     session = frida.get_usb_device().attach(pid)
+            #     hook = open(method, 'r')
+            #     script = session.create_script(hook.read())
+            #     script.load()
+            #     frida.get_usb_device().resume(pid)
+            #     sys.stdin.read()
+            # else:
+            #     logger.error('[?] Script for method not found!')
+
+        #Bypass SSL Pinning
+        elif options.package and options.method == "bypass-ssl":
+            method = methods[2]
+            logger.warning('[!] The Method Is Updating!!')
+            # if os.path.isfile(method):
+            #     logger.info('[*] Bypass SSL Pinning: ')
+            #     logger.info('[*] Spawning: ' + options.package)
+            #     logger.info('[*] Script: ' + method)
+            #     os.system('frida -U -f '+ options.package + ' -l ' + method + ' --no-pause')
+            #     #sys.stdin.read()
+            # else:
+            #     logger.error('[?] Script for method not found!')
+
+        #Intercept url request in app
+        elif options.name and options.method == "i-url-req":
+            method = methods[4]
+            logger.warning('[!] The Method Is Updating!!')
+            # if os.path.isfile(method):
+            #     logger.info('[*] Intercept UrlRequest: ')
+            #     logger.info('[*] Attaching: ' + options.name)
+            #     logger.info('[*] Script: ' + method)
+            #     time.sleep(2)
+            #     process = frida.get_usb_device().attach(options.name)
+            #     method = open(method, 'r')
+            #     script = process.create_script(method.read())
+            #     script.load()
+            #     sys.stdin.read()
+            # else:
+            #     logger.error('[?] Script for method not found!')
+
+        #Intercept Crypto Operations
+        elif options.package and options.method == "i-crypto":
+            method = methods[5]
+            logger.warning('[!] The Method Is Updating!!')
+            # if os.path.isfile(method):
+            #     logger.info('[*] Intercept Crypto Operations: ')
+            #     logger.info('[*] Spawning: ' + options.package)
+            #     logger.info('[*] Script: ' + method)
+            #     os.system('frida -U -f '+ options.package + ' -l ' + method + ' --no-pause')
+            #     #sys.stdin.read()
+            # else:
+            #     logger.error('[?] Script for method not found!')
+
+        #check newversion
+        elif options.checkversion:
+            logger.info('[*] Checking for updates...')
+            is_newest = check_version(speak=True)
+            # if not is_newest:
+            #     logger.info('[*] There is an update available for iOS hook')
+
+        #update newversion
+        elif options.update:
+            logger.info('[*] Update in progress...')
+            cmd = shlex.split("git pull origin master")
+            subprocess.call(cmd)
             sys.exit(0)
+
+        #dump decrypt application
+        elif options.dump:
+            logger.warning('[!] The Method Is Updating!!')
 
         else:
-            print("[-] specify the options. use (-h) for more help!")
-            sys.exit(0)
+            logger.warning("[!] Specify the options. use (-h) for more help!")
+            # sys.exit(0)
+
+    #EXCEPTION FOR FRIDA
+    except frida.ServerNotRunningError:
+        logger.error("Frida server is not running.")
+    except frida.TimedOutError:
+        logger.error("Timed out while waiting for device to appear.")
+    except frida.TransportError:
+        logger.error("[x_x] The application may crash or lose connection.")
+    except (frida.ProcessNotFoundError,
+            frida.InvalidOperationError):
+        logger.error("[x_x] Unable to find process with name " + options.name + ". You need run app first.!!")
+    #EXCEPTION FOR OPTIONPARSING
+
+    #EXCEPTION FOR SYSTEM
+    except Exception as e:
+        logger.error("[x_x] Something went wrong, please check your error message.\n Message - {0}".format(e))
 
     except KeyboardInterrupt:
-        print("[:)] Bye!!")
-        sys.exit(0)
+        logger.info("Bye bro!!")
+        # sys.exit(0)
 
 if __name__ == '__main__':
-        main()
+        run()
 
     
