ff34066e1de286c59132d97bfd8e34e79857806b
noobpk <ltp.noobpk@gmail.com>
2021-02-05 15:34:43 +0700

    update frida-scritp

---
+++ frida-scripts/raptor_frida_android_findClass1.js
@@ -0,0 +1,42 @@
+/*
+ * raptor_frida_android_*.js - Frida snippets for Android
+ * Copyright (c) 2017 Marco Ivaldi <raptor@0xdeadbeef.info>
+ *
+ * Frida.re JS script snippets for Android instrumentation.
+ * See https://www.frida.re/ and https://codeshare.frida.re/
+ * for further information on this powerful tool.
+ *
+ * "We want to help others achieve interop through reverse
+ * engineering" -- @oleavr
+ *
+ * Many thanks to Maurizio Agazzini <inode@wayreth.eu.org>
+ * and Federico Dotta <federico.dotta@mediaservice.net>.
+ *
+ * Example usage:
+ * # frida -U -f com.xxx.yyy -l raptor_frida_android.js --no-pause
+ */
+
+// find loaded classes that match a pattern (sync)
+function findClass(pattern)
+{
+	console.warn("\n*** finding all classes that match pattern: " + pattern + "\n");
+
+	var allClasses = Java.enumerateLoadedClassesSync();
+	allClasses.forEach(function(aClass) {
+		if (aClass.match(pattern))
+			console.log(aClass);
+	});
+}
+
+// usage examples
+setTimeout(function() { // avoid java.lang.ClassNotFoundException
+
+	Java.perform(function() {
+
+		//findClass();		// print all loaded classes
+		//findClass("Root");	// print all classes that match a string
+		//findClass(/root/i);	// print all classes that match a regex (e.g., case insensitive)
+
+	});   
+
+}, 0);
